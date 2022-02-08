1272734644bb6dc277e1597cd94989102fcd451b
ga249 <ga249@njit.edu>
2022-02-08 10:40:36 -0500

    Updated Name

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Gabe"
+        name = "Gibby A"
         return render_template('index.html', name=name)
